7cf48aa37d93ecdff82b88de2ef46fc5644dd574
k1m2njun <k1m2njun@naver.com>
2023-06-20 16:01:00 +0900

    init Project

---
+++ src/test/java/com/k1m2njun/getinline/GetInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k1m2njun.getinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
