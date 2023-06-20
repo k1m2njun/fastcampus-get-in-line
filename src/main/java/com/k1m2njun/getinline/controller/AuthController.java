775ab212a78ae349922e0e1e133910d04261f62e
k1m2njun <k1m2njun@naver.com>
2023-06-20 16:57:59 +0900

    Control Error page, and Create logIn, signUp page

--- src/main/java/com/k1m2njun/getinline/controller/AuthController.java
+++ src/main/java/com/k1m2njun/getinline/controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.k1m2njun.getinline.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String logIn() {
+        return "auth/login";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUp() {
+        return "auth/sign-up";
+    }
 }
--- src/main/java/com/k1m2njun/getinline/controller/BaseController.java
+++ src/main/java/com/k1m2njun/getinline/controller/BaseController.java
@@ -1,13 +1,20 @@
 package com.k1m2njun.getinline.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController {
+public class BaseController implements ErrorController { // 에러 페이지 제어권을 가지려면 ErrorController를 구현해야 함.
 
     @GetMapping("/")
     public String root() {
         return "index"; // Controller는 index에 해당하는 view를 찾아서 렌더링해서 화면에 보여준다.
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
     }
 }
